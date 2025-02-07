c68fd45ef2ef55a24f8f47b78bce8b1958ec0f67
Jeremy Spradlin <jeremy.spradlin@kbi.ai>
2025-02-06 19:35:17 -0500

    Fixed vite config error

--- synapse/vite.config.ts
+++ synapse/vite.config.ts
@@ -1,9 +1,6 @@
 import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
 
-// @ts-expect-error process is a nodejs global
-const host = process.env.TAURI_DEV_HOST;
-
 // https://vitejs.dev/config/
 export default defineConfig(async () => ({
   plugins: [react()],
